1db71cbca28a58b541424fcad399b10dca8efb8b
hippogriff101 <143739931+hippogriff101@users.noreply.github.com>
2026-03-12 21:12:04 +0000

    Update lives tracking and enhance death message in u_dead.gd

--- scripts/u_dead.gd
+++ scripts/u_dead.gd
@@ -1,12 +1,14 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+var lives: int = 5
 
 func _on_body_entered(_body: Node2D) -> void:
-	print("you died")
+	lives -= 1
 	timer.start()
 
 func _on_timer_timeout() -> void:
+	print("you died: ", lives, " lives left")
 	var p = AudioStreamPlayer.new()
 	p.stream = preload("res://assets/brackeys_platformer_assets/sounds/hurt.wav")
 	get_tree().current_scene.add_child(p)
